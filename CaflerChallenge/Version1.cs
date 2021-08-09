cb1319cbc68ba75e8ca1367f1270f1748b6399a9
Uriyeah55 <74713703+Uriyeah55@users.noreply.github.com>
2021-08-09 12:34:43 +0200

    1

---
+++ CaflerChallenge/Version1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CaflerChallenge
+{
+    class Version1
+
+    {
+        private static void Pep (string[] args)
+        {
+            int MAX_NUMBER = 1000;
+
+            Console.WriteLine("¿How many validations will be checked?");
+            int validationsNum = Int32.Parse(Console.ReadLine());
+
+            int[] num = new int[validationsNum];
+            string[] str = new string[validationsNum];
+
+            for (int cont = 0; cont < validationsNum; cont++)
+            {
+                Console.WriteLine("Assign a number");
+                num[cont] = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Assign a string to numbers divisible by said number");
+                str[cont] = Console.ReadLine();
+            }
+
+            for (int cont = 1; cont <= 1000; cont++)
+            {
+                string output = "";
+
+                for (int currentNumber = 0; currentNumber < validationsNum; currentNumber++)
+                {
+                    if (cont % num[currentNumber] == 0)
+                    {
+                        output += str[currentNumber];
+                    }
+                }
+                if (output == "")
+                {
+                    output += cont.ToString();
+                }
+
+                Console.WriteLine(output);
+            }
+
+        }
+    }
+}
+    
+
---
+++ CaflerChallenge/Version2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CaflerTest3
+{
+    class Version2
+    {
+        static int MAX_VALUE = 50;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number of validations:");
+            int valNumber = Convert.ToInt32(Console.ReadLine());
+
+            List<int> validList = new List<int>();
+            List<string> valStringList = new List<string>();
+            for (int i = 0; i < valNumber; i++)
+            {
+                Console.WriteLine("Checking number " + (i + 1) + "to validate");
+                int num = Convert.ToInt32(Console.ReadLine());
+                validList.Add(num);
+                Console.WriteLine("Word to show:");
+                string word = Console.ReadLine();
+                valStringList.Add(word);
+            }
+            Console.WriteLine("==========Counting=======");
+
+            for (int p = 1; p <= MAX_VALUE; p++)
+            {
+                bool found = false;
+                String msg = "";
+                foreach (int number in validList)
+                {
+
+
+                    if (p % number == 0)
+                    {
+                        int pus = validList.IndexOf(number);
+                        msg += valStringList[pus];
+                        found = true;
+                    }
+                }
+                if (found)
+                {
+                    Console.WriteLine(msg);
+                }
+                else
+                {
+                    Console.WriteLine(p);
+                }
+            }
+        }
+    }
+}
